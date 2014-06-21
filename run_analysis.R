2454e840373ea1265e40c9f0824dd1b324619f6f
Xuening Wu <xueningwu@gmail.com>
2014-06-21 15:19:14 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,5 @@
 setwd("c:\\temp")
-library("reshap2")
+
 # STEP 1: read data training,test and features
 trainingX <- read.table(".\\project\\train\\X_train.txt")
 trainingY <- read.table(".\\project\\train\\y_train.txt")
@@ -20,9 +20,11 @@ allSubject <- rbind(trainingSubject,testSubject)
 # STEP 2: Find the mean() and std() features and flag them and subset 
 features$flag<-grepl('mean\\(\\)|std\\(\\)',features[,"V2"])
 features_mean_std<-subset(features,features$flag==TRUE,select=c(V1,V2))
+features_mean_std$V2<-gsub("\\(\\)", "", features_mean_std$V2)  # remove "()" 
 
 # select mean and std columns from features_mean_std with X,Y and Subject
-mean_std_X<-allX[,features_mean_std$V2]
+mean_std_X<-allX[,features_mean_std$V1]
+
 Y_Subject <- cbind(allY,allSubject)
 colnames(Y_Subject)<-c("Y","subject")
 
@@ -32,12 +34,12 @@ colnames(mean_std_X)<-features_mean_std$V2
 # STEP 3:
 
 rowname=merge(Y_Subject,activity,by.x="Y",by.y="V1",all=FALSE)
-rowname$SubjectActionID<-paste(as.factor(rowname$subject),as.factor(rowname$V2))
-final<-cbind(rowname$SubjectActionID,mean_std_X)
+colnames(rowname)<-c("Y","subject","activity")
+final<-cbind(rowname,mean_std_X)
 
 #STEP 5
-
-Molten <- melt(, id.vars=("activity","subject"))
-
+library("reshap2")
+melt.final<-melt(final,id=c("subject","activity"),measure.vars=features_mean_std$V2)
+cast.final <- dcast(melt.final,subject+activity ~variable,mean)
 #output
-write.table(final,file=".\\project\\final.csv",sep=" ")
+write.table(cast.final,file=".\\project\\final.txt",sep=" ")
